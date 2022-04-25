8d7d13aa32189ff1d82f0694910faae4b34c4b8a
lmaupas <louis.maupas@framatome.com>
2022-04-25 17:08:20 +0200

    :style: (Dashboard) misc ui

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useUserData } from "../../utils/hooks/customHooks";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Dashboard() {
   const userData = useUserData();
